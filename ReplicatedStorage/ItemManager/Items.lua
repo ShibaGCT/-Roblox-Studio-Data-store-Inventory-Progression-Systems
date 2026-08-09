9fb828df360ca6805c2797fd5f65b13238e79e7d
ShibaGCT <nattapatjskzz@gmail.com>
2026-08-09 22:00:57 +0700

    Add Items.lua to manage game items

---
+++ ReplicatedStorage/ItemManager/Items.lua
@@ -0,0 +1,18 @@
+local Items = {
+	["Item1"] = {
+		Image = "rbxassetid://YOUR_IMAGE_ID",
+		Rarity = "Uncommon"
+	},
+	["Item2"] = {
+		Image = "rbxassetid://YOUR_IMAGE_ID",
+		Rarity = "Rare"
+	},
+  ["Item3"] = {
+		Image = "rbxassetid://YOUR_IMAGE_ID",
+		Rarity = "Epic"
+	}
+}
+
+return Items
+
+
